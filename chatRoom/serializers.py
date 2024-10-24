e87311f60dc34d285faacd859b68440afb44d554
iwantcoke <1916771180@qq.com>
2024-10-24 23:04:11 +0800

    点赞收藏后端

--- chatRoom/serializers.py
+++ chatRoom/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from chatRoom.models import Discussion, Review, PictureDisscussion, PictureReview
+from chatRoom.models import Discussion, Review, PictureDisscussion, PictureReview, Favorite
 
 
 class discussionSerializer(serializers.ModelSerializer):
@@ -38,3 +38,34 @@ class PictureDisscussionSerializer(serializers.ModelSerializer):
     class Meta:
         model = PictureDisscussion
         fields = ['pfile']
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    dis_detail = serializers.SerializerMethodField()
+    class Meta:
+        model = Favorite
+        fields = '__all__'
+    def get_dis_detail(self, obj):
+        discussion = obj.dno
+        return discussionDetailSerializer(discussion).data
+
+# 收藏夹就不用预览图片了，就展示标题和正文，打开详细信息就跳转到课程展示的那一段逻辑{
+#     "code": 200,
+#     "data": [
+#         {
+#             "id": 4,
+#             "dis_detail": {
+#                 "dno": 20,
+#                 "pictures": [],
+#                 "dtitle": "课程2测试",
+#                 "dinfo": "！",
+#                 "postTime": "2024-10-23T01:08:43",
+#                 "updateTime": "2024-10-23T01:08:45",
+#                 "havePic": false,
+#                 "cno": "0002",
+#                 "ownerNo": 3
+#             },
+#             "dno": 20,
+#             "userNo": 2
+#         }
+#     ]
+# }
